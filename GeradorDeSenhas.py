b14fa8860784c748c7bcd5438b77e07b758ad2b5
josanjohnata <josanjohnata@gmail.com>
2021-08-08 21:11:58 -0300

    Adiciona layout da aplicaçao

--- GeradorDeSenhas.py
+++ GeradorDeSenhas.py
@@ -4,7 +4,18 @@ import os
 
 class PassGen:
   def __init__(self):
-    pass
+    # Declarando o Layout
+    sg.theme('Black')
+    layout = [
+      [sg.Text('Site/Software', sizer=(10, 1)),
+       sg.Input(key='site', size=(20, 1))],
+      [sg.Text('E-mail/Usuário', sizer=(10, 1)),
+       sg.Input(key='usuario', sizer=(20, 1))],
+      [sg.Text('Quantidade de caracteres'), sg.combo(values=list(
+        range(30)), key='total_chars', default_value=1, size=(3, 1))],
+      [sg.Output(size=(32, 5))],
+      [sg.Button('Gerar Senha')]
+    ]
 
   def Iniciar(self):
     pass
